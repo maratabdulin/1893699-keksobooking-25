dc872c73c473e560b02b10e935d7377acd0d9788
Marat <marato13@ya.ru>
2022-02-19 20:32:27 +0300

    Delete console.log

--- js/main.js
+++ js/main.js
@@ -2,9 +2,7 @@ const getRandomInt = (minNumber, maxNumber) => {
   if (maxNumber - minNumber < 0) {
     return null;
   }
-  const max = Math.floor(maxNumber);
-  const min = Math.ceil(minNumber);
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+  return Math.floor(Math.random() * (maxNumber - minNumber + 1) ) + minNumber;
 };
 
 const getRandomFloat = (minNumber, maxNumber, fractionDigits = 0) => {
@@ -14,10 +12,7 @@ const getRandomFloat = (minNumber, maxNumber, fractionDigits = 0) => {
   return  +(Math.random() * (maxNumber - minNumber) + minNumber).toFixed(fractionDigits);
 };
 
-const result1 = getRandomInt(2,15);
-const result2 = getRandomInt(12,2);
-const result3 = getRandomFloat(2,15, 4);
-const result4 = getRandomFloat(12,2);
-const result5 = getRandomFloat(0,3, 2);
-
-console.log(result1, result2, result3, result4, result5);
+getRandomInt(2, 15);
+getRandomInt(12, 2);
+getRandomFloat(2,15, 4);
+getRandomFloat(12,2);

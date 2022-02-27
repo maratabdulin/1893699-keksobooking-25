2e00557679260b08a694314192aaf7518b60b52e
Marat <marato13@ya.ru>
2022-02-27 12:06:52 +0300

    get function from solution

--- js/main.js
+++ js/main.js
@@ -1,18 +1,13 @@
-const getRandomInt = (minNumber, maxNumber) => {
-  if (maxNumber - minNumber < 0) {
-    return null;
-  }
-  return Math.floor(Math.random() * (maxNumber - minNumber + 1) ) + minNumber;
-};
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
 
-const getRandomFloat = (minNumber, maxNumber, fractionDigits = 0) => {
-  if (maxNumber - minNumber < 0) {
-    return null;
-  }
-  return  +(Math.random() * (maxNumber - minNumber) + minNumber).toFixed(fractionDigits);
-};
-
-getRandomInt(2, 15);
-getRandomInt(12, 2);
-getRandomFloat(2,15, 4);
-getRandomFloat(12,2);
+function getRandomPositiveFloat (a, b, digits = 1) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+  return +result.toFixed(digits);
+}
